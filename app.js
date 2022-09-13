796e2ac47c3faa2573d3c05f0011d70241423142
dphuocsw <duongdangphuoc.bk@gmail.com>
2022-09-13 23:21:17 +0700

    update changed

--- app.js
+++ app.js
@@ -8,5 +8,20 @@ function start() {
     renderUsers();
 }
 function renderUsers() {
-
+    $.ajax({
+        type: 'GET',
+          dataType:"jsonp",
+        url: 'https://jsonplaceholder.typicode.com/todos/1',
+        headers:{         
+            'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBWZXIiOiIwLjAuMCIsImV4cCI6NDcyNjM4OTEyMiwibG9jYWxlIjoiIiwibWFzdGVyVmVyIjoiIiwicGxhdGZvcm0iOiIiLCJwbGF0Zm9ybVZlciI6IiIsInVzZXJJZCI6IiJ9.QIZbmB5_9Xlap_gDhjETfMI6EAmR15yBtIQkWFWJkrg',
+            
+        },
+        success: function (data, status, xhr) {
+            
+        }
+      });
+};
+function getUsersHandle(data) {
+    let html;
+    
 }
