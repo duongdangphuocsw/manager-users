2286deaf60bd1962fb56c22c365a45351395d523
dphuocsw <duongdangphuoc.bk@gmail.com>
2022-09-13 20:36:46 +0700

    update changed

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const $$ = document.querySelectorAll.bind(document);
 
 import clickItemHandle from './header.js';
 clickItemHandle();
+const usersApi = "https://631c255e4fa7d3264ca7c5ca.mockapi.io/api/users";
+function start() {
+    renderUsers();
+}
+function renderUsers() {
+
+}
